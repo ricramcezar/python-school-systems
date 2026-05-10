40a7fca63a69587b9d5b7bf42a5c43c43f3f38e8
ricramcezar <ricardocezarconsultoria@gmail.com>
2026-05-09 21:37:50 -0300

    Acréscimo da validação das notas

--- 01_boletim_simples/boletim.py
+++ 01_boletim_simples/boletim.py
@@ -4,9 +4,24 @@ while True:
     nome = input("Digite o nome do aluno: ")
 
     nota1 = float(input("Digite a primeira nota: "))
+    while nota1 < 0 or nota1 > 10:
+        print("Nota inválida. Digite uma nota entre 0 e 10.")
+        nota1 = float(input("Digite a primeira nota: "))
+    
     nota2 = float(input("Digite a segunda nota: "))
+    while nota2 < 0 or nota2 > 10:
+        print("Nota inválida. Digite uma nota entre 0 e 10.") 
+        nota2 = float(input("Digite a segunda nota: "))
+    
     nota3 = float(input("Digite a terceira nota: "))
+    while nota3 < 0 or nota3 > 10:
+        print("Nota inválida. Digite uma nota entre 0 e 10.")
+        nota3 = float(input("Digite a terceira nota: "))
+    
     nota4 = float(input("Digite a quarta nota: "))
+    while nota4 < 0 or nota4 > 10:
+        print("Nota inválida. Digite uma nota entre 0 e 10.") 
+        nota4 = float(input("Digite a quarta nota: "))
 
     media = (nota1 + nota2 + nota3 + nota4) / 4
 
